c6af9c5ac13b165e92e35116ed080a14efc1dab0
Robert <46203504+rdweston1995@users.noreply.github.com>
2019-08-06 18:20:17 -0700

    Added a few components and started json file

---
+++ src/components/Header/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./style.css";
+
+function Header(){
+    return (
+        <header>
+            <h1>TBD</h1>
+            <h2>Click an image to earn points, but don't click on any more than once!</h2>
+        </header>
+    );
+}
+
+export default Header;
---
+++ src/components/container/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./style.css"
+
+function Container(props){
+    return (
+        <main className="Container" {...props}/>
+    )
+}
+
+export default Container;
